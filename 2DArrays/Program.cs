5bd47d41fbea66accf959f9d13003ca3d451a2d7
Raptarior <qazqww@naver.com>
2021-02-19 17:40:08 +0900

    210219
    Day30 - 10, 11일차

---
+++ 2DArrays/Program.cs
@@ -0,0 +1,40 @@
+﻿// https://www.hackerrank.com/challenges/30-2d-arrays/problem
+
+using System;
+
+namespace _2DArrays
+{
+    class Solution {
+    static void Main(string[] args) {
+        int[][] arr = new int[6][];
+        int maxNum = -10000;
+
+        for (int i = 0; i < 6; i++) {
+            arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+        }
+        
+        for (int i = 0; i < arr.GetLength(0) - 2; i++)
+        {
+            for (int j = 0; j < arr[0].GetLength(0) - 2; j++)
+            {
+                try
+                {
+                    int tempNum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2]
+                        + arr[i + 1][j + 1]
+                        + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+                    
+                    if (tempNum > maxNum)
+                    {
+                        maxNum = tempNum;
+                    }
+                    
+                } catch(Exception e) {
+                    continue;
+                }    
+            }
+        }
+        
+        Console.WriteLine(maxNum);
+    }
+}
+}
---
+++ BinaryNumbers/Program.cs
@@ -0,0 +1,40 @@
+﻿// https://www.hackerrank.com/challenges/30-binary-numbers/problem
+
+using System;
+
+namespace BinaryNumbers
+{
+    class Solution
+    {
+        static int CountOneInBinary(int num)
+        {
+            int countOne = 0;
+            int tempOne = 0;
+
+            while (num > 0)
+            {
+                if (num % 2 == 1)
+                {
+                    tempOne++;
+                    if (tempOne > countOne)
+                    {
+                        countOne = tempOne;
+                    }
+                }
+                else
+                {
+                    tempOne = 0;
+                }
+                num /= 2;
+            }
+
+            return countOne;
+        }
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(CountOneInBinary(n));
+        }
+    }
+}
