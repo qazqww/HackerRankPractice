461cd841f585866d64f69e15c166ca7cf691dee3
Raptarior <qazqww@naver.com>
2021-02-22 15:13:58 +0900

    210222
    Day30 - 14, 15일차

---
+++ Scope/Program.cs
@@ -0,0 +1,31 @@
+﻿// https://www.hackerrank.com/challenges/30-scope/problem
+
+using System;
+using System.Linq;
+
+class Difference {
+    private int[] elements;
+    public int maximumDifference;
+
+    public Difference(int[] elements) => this.elements = elements;
+    
+    public void computeDifference()
+    {
+        Array.Sort(elements);
+        maximumDifference = elements[elements.Length - 1] - elements[0];
+    }
+}
+
+class Solution {
+    static void Main(string[] args) {
+        Convert.ToInt32(Console.ReadLine());
+        
+        int[] a = Console.ReadLine().Split(' ').Select(x=>Convert.ToInt32(x)).ToArray();
+        
+        Difference d = new Difference(a);
+        
+        d.computeDifference();
+        
+        Console.Write(d.maximumDifference);
+    }
+}
