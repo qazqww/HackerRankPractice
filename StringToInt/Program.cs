957e699024a97257cef9ded7f3d5443ed4c09d61
Raptarior <qazqww@naver.com>
2021-02-23 11:04:02 +0900

    210223
    Day30 - 16, 17일차

---
+++ StringToInt/Program.cs
@@ -0,0 +1,19 @@
+﻿// https://www.hackerrank.com/challenges/30-exceptions-string-to-integer/problem
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution {
+    static void Main(String[] args) {
+        string S = Console.ReadLine();
+
+        try {
+            Console.WriteLine(int.Parse(S));
+        }
+        catch (Exception e) {
+            Console.WriteLine("Bad String");
+        }
+    }
+}
