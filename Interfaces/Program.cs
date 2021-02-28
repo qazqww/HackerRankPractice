bd94d66d0dd2f5ac86a88d9c26d6786e08edee6a
Raptarior <qazqww@naver.com>
2021-02-28 12:35:28 +0900

    210228
    Day30 - 18, 19, 20일차

---
+++ Interfaces/Program.cs
@@ -0,0 +1,34 @@
+﻿// https://www.hackerrank.com/challenges/30-interfaces/problem
+
+using System;
+
+public interface AdvancedArithmetic{
+    int divisorSum(int n);
+}
+
+public class Calculator : AdvancedArithmetic
+{
+    public int divisorSum(int n)
+    {
+        int result = 0;
+
+        for (int i = 1; i <= n / 2; i++)
+        {
+            if (n % i == 0)
+            {
+                result += i;
+            }
+        }
+
+        return result + n;
+    }
+}
+
+class Solution{
+    static void Main(string[] args){
+        int n = Int32.Parse(Console.ReadLine());
+      	AdvancedArithmetic myCalculator = new Calculator();
+        int sum = myCalculator.divisorSum(n);
+        Console.WriteLine("I implemented: AdvancedArithmetic\n" + sum); 
+    }
+}
---
+++ Sorting/Program.cs
@@ -0,0 +1,41 @@
+﻿// https://www.hackerrank.com/challenges/30-sorting/problem
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+    static void swap(ref int a, ref int b)
+    {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] a_temp = Console.ReadLine().Split(' ');
+        int[] a = Array.ConvertAll(a_temp, Int32.Parse);
+        // Write Your Code Here
+        int swapCount = 0;
+
+        for (int i = 0; i < a.Length; i++)
+        {
+            for (int j = 0; j < a.Length - 1 - i; j++)
+            {
+                if (a[j] > a[j+1])
+                {
+                    swap(ref a[j], ref a[j+1]);
+                    swapCount++;
+                }
+            }
+        }
+
+        Console.WriteLine($"Array is sorted in {swapCount} swaps.");
+        Console.WriteLine($"First Element: {a[0]}");
+        Console.WriteLine($"Last Element: {a[a.Length-1]}");
+    }
+}
