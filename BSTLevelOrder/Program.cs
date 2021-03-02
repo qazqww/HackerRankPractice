b0798073e1101b43cdeb51429919c9792bcd3a07
Raptarior <qazqww@naver.com>
2021-03-02 14:56:40 +0900

    210302
    Day30 - 23, 24일차

---
+++ BSTLevelOrder/Program.cs
@@ -0,0 +1,64 @@
+﻿// https://www.hackerrank.com/challenges/30-binary-trees/problem
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Node{
+    public Node left,right;
+    public int data;
+    public Node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
+class Solution{
+	static void levelOrder(Node root){
+  	    //Write your code here
+        Queue<Node> queue = new Queue<Node>();
+        queue.Enqueue(root);
+
+        while (queue.Count != 0)
+        {
+            Node temp = queue.Dequeue();
+            Console.Write(temp.data + " ");
+            if (temp.left != null)
+            {
+                queue.Enqueue(temp.left);
+            }
+            if (temp.right != null)
+            {
+                queue.Enqueue(temp.right);
+            }
+        }
+    }
+
+	static Node insert(Node root, int data){
+        if(root==null){
+            return new Node(data);
+        }
+        else{
+            Node cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
+        }
+    }
+    static void Main(String[] args){
+        Node root=null;
+        int T=Int32.Parse(Console.ReadLine());
+        while(T-->0){
+            int data=Int32.Parse(Console.ReadLine());
+            root=insert(root,data);            
+        }
+        levelOrder(root);
+        
+    }
+}
---
+++ MoreLinkedLists/Program.cs
@@ -0,0 +1,78 @@
+﻿// https://www.hackerrank.com/challenges/30-linked-list-deletion/problem
+
+using System;
+using System.Collections.Generic;
+class Node
+{
+	public int data;
+	public Node next;
+    public Node(int d){
+        data=d;
+        next=null;
+    }
+		
+}
+class Solution {
+
+    public static Node removeDuplicates(Node head){
+        //Write your code here
+        if (head == null)
+        {
+            return null;
+        }
+
+        Node newHead = new Node(head.data);
+        Node start = newHead;
+        while (head.next != null)
+        {
+            if (head.data != head.next.data)
+            {
+                start.next = new Node(head.next.data);
+                start = start.next;
+            }
+            head = head.next;
+        }
+
+        return newHead;
+    }
+
+	public static Node insert(Node head,int data)
+	{
+        Node p=new Node(data);
+		
+		
+		if(head==null)
+			head=p;
+		else if(head.next==null)
+			head.next=p;
+		else
+		{
+			Node start=head;
+			while(start.next!=null)
+				start=start.next;
+			start.next=p;
+			
+		}
+		return head;
+    }
+	public static void display(Node head)
+	{
+		Node start=head;
+		while(start!=null)
+		{
+			Console.Write(start.data+" ");
+			start=start.next;
+		}
+	}
+    static void Main(String[] args) {	
+		Node head=null;
+        int T=Int32.Parse(Console.ReadLine());
+        while(T-->0){
+            int data=Int32.Parse(Console.ReadLine());
+            head=insert(head,data);
+        }
+      	head=removeDuplicates(head);
+		display(head);
+	}
+}
+	
