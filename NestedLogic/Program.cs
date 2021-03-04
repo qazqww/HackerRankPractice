876fc0c19fe4fe6b938062abb96e0807ef6f320e
Raptarior <qazqww@naver.com>
2021-03-04 13:35:54 +0900

    210304
    Day30 - 25, 26일차

---
+++ NestedLogic/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace NestedLogic
+{
+    class Solution
+    {
+        static int FineState(int[] returned, int[] due)
+        {
+            if (returned[2] < due[2])
+            {
+                return 0;
+            }
+            else if (returned[2] > due[2])
+            {
+                return 10000;
+            }
+            else if (returned[1] > due[1])
+            {
+                return 500 * (returned[1] - due[1]);
+            }
+            else if (returned[0] > due[0])
+            {
+                return 15 * (returned[0] - due[0]);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string[] returned_temp = Console.ReadLine().Split(' '); // [0]day [1]month [2]year
+            int[] returned = Array.ConvertAll(returned_temp, Int32.Parse);
+            string[] due_temp = Console.ReadLine().Split(' ');
+            int[] due = Array.ConvertAll(due_temp, Int32.Parse);
+
+            Console.WriteLine(FineState(returned, due));
+        }
+    }
+}
---
+++ RunningTimeComplexity/Program.cs
@@ -0,0 +1,40 @@
+﻿// https://www.hackerrank.com/challenges/30-running-time-and-complexity/problem
+
+using System;
+
+namespace RunningTimeComplexity
+{
+    class Solution
+    {
+        static void isPrime(int num)
+        {
+            if (num == 1)
+            {
+                Console.WriteLine("Not prime");
+                return;
+            }
+
+            int i = 1;
+            while (++i <= Math.Sqrt(num))
+            {
+                if (num % i == 0)
+                {
+                    Console.WriteLine("Not prime");
+                    return;
+                }                
+            }
+
+            Console.WriteLine("Prime");
+        }
+
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            while(count-- > 0)
+            {
+                int num = int.Parse(Console.ReadLine());
+                Solution.isPrime(num);
+            }
+        }
+    }
+}
