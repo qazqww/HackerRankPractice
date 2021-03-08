458f75893e010a0c7653eef7e4ecaafe571c20d0
Raptarior <qazqww@naver.com>
2021-03-08 20:11:24 +0900

    210308
    Day30 - 27, 28, 29일차

---
+++ BitwiseAND/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int t = Convert.ToInt32(Console.ReadLine());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            string[] nk = Console.ReadLine().Split(' ');
+            int n = Convert.ToInt32(nk[0]);
+            int k = Convert.ToInt32(nk[1]);
+
+            int maxNum = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = i + 1; j <= n; j++)
+                {
+                    if ((i & j) > maxNum && (i & j) < k)
+                    {
+                        maxNum = i & j;                        
+                    }
+                }
+            }
+
+            Console.WriteLine(maxNum);
+        }
+    }
+}
