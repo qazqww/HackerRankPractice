662f9d0ccb5651c70f0b9d18061ff898e1fcf2f5
Raptarior <qazqww@naver.com>
2021-03-10 19:36:00 +0900

    210310 - Warmup

---
+++ AlgorithmSolving/PlusMinus/Program.cs
@@ -0,0 +1,24 @@
+﻿// https://www.hackerrank.com/challenges/plus-minus/problem
+
+using System;
+using System.Linq;
+
+class Solution
+{
+    // Complete the plusMinus function below.
+    static void plusMinus(int[] arr)
+    {
+        double posRatio = (double)arr.Where(i => i > 0).Count() / arr.Length;
+        double negRatio = (double)arr.Where(i => i < 0).Count() / arr.Length;
+        double zeroRatio = (double)arr.Where(i => i == 0).Count() / arr.Length;
+        Console.WriteLine($"{posRatio}\n{negRatio}\n{zeroRatio}");
+    }
+
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+        plusMinus(arr);
+    }
+}
---
+++ AlgorithmSolving/TimeConversion/Program.cs
@@ -0,0 +1,40 @@
+﻿// https://www.hackerrank.com/challenges/time-conversion/problem
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+    /*
+     * Complete the timeConversion function below.
+     */
+    static string timeConversion(string s)
+    {
+        /*
+         * Write your code here.
+         */
+        if (s.Contains("PM") && s.Substring(0, 2) != "12")
+        {
+            string newHour = (int.Parse(s.Substring(0, 2)) + 12).ToString();
+            return newHour + s.Substring(2, s.Length - 4);
+        }
+        else if (s.Contains("AM") && s.Substring(0, 2) == "12")
+        {
+            return "00" + s.Substring(2, s.Length - 4);
+        }
+        else
+        {
+            return s.Substring(0, s.Length - 2);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        string s = Console.ReadLine();
+
+        string result = timeConversion(s);
+        Console.WriteLine(result);
+    }
+}
